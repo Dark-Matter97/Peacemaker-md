c10b004b27125d47c40e19b95991f602e45f6eb3
Peacemaker Tech <muuoemmanuel649@gmail.com>
2025-05-23 05:41:37 +0300

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -63,7 +63,7 @@ async (conn, mek, m, { from, reply, sender, args }) => {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363290715861418@newsletter',
-                        newsletterName: 'PopkidXtech',
+                        newsletterName: 'Peacemaker',
                         serverMessageId: 143
                     }
                 }
